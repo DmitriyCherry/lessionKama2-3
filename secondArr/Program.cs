7abec125d156c0ca56e083b1a24c853c3690caa5
Dmitriy <dmitr.wischnewski@mail.ru>
2023-02-07 11:21:11 +0300

    Нашли нужный индекс в массиве

---
+++ secondArr/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = { 12, 23, 43, 54, 35, 56, 756, 876, 32, 11, 2 };
+
+int n = array.Length;
+int find = 56; 
+int index = 0;
+
+while(index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++; 
+}
